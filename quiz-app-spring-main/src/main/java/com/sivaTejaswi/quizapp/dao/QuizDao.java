3b5be345506c171dfc1652442c5fcbf79a0b1cf3
sivatejaswi27 <114803924+sivatejaswi27@users.noreply.github.com>
2024-07-23 11:46:53 +0530

    Add files via upload

---
+++ quiz-app-spring-main/src/main/java/com/sivaTejaswi/quizapp/dao/QuizDao.java
@@ -0,0 +1,7 @@
+package com.sivaTejaswi.quizapp.dao;
+
+import com.telusko.quizapp.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizDao extends JpaRepository<Quiz,Integer> {
+}
