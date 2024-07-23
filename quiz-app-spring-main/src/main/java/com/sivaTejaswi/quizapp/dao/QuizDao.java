ce52fb96963a90fe62e7ecae1c28f756f99c292d
sivatejaswi27 <114803924+sivatejaswi27@users.noreply.github.com>
2024-07-23 11:45:55 +0530

    Delete quiz-app-spring-main directory

--- quiz-app-spring-main/src/main/java/com/sivaTejaswi/quizapp/dao/QuizDao.java
+++
@@ -1,7 +0,0 @@
-package com.sivaTejaswi.quizapp.dao;
-
-import com.sivaTejaswi.quizapp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz,Integer> {
-}
